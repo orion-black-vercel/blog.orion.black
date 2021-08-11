6c79e3355ef0b37afde32ee46ee76751e94a89bd
アレクサンダー U B I Q U I T Y <gpg@pavlovcik.com>
2021-08-11 00:59:25 -0400

    add(github link): in social icons

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
     // social usernames (optional)
     // twitter: '',
-    // github: '',
+    github: 'orion-black-tech',
     // linkedin: 'fisch2',
 
     // default notion icon and cover images for site-wide consistency (optional)
